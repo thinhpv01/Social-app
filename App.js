4659f7776353e5e314d0ef7f97f4bde406ed73d0
Thinh Pham <phamvanthinh2001x@gmail.com>
2022-11-29 15:52:28 +0700

    🎉 Header and BottomTab

--- App.js
+++ App.js
@@ -1,20 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import {
+  CardStyleInterpolators,
+  createStackNavigator,
+} from "@react-navigation/stack";
+import { StatusBar } from "expo-status-bar";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import Feeds from "./src/screens/Feeds";
+import Profile from "./src/screens/Profile";
+
+const Stack = createStackNavigator();
+
+const MyStack = () => {
+  return (
+    <Stack.Navigator
+      screenOptions={{
+        gestureEnabled: true,
+        gestureDirection: "horizontal",
+        cardStyleInterpolator: CardStyleInterpolators.forHorizontalIOS,
+        headerMode: "float",
+        headerShown: false,
+      }}
+    >
+      <Stack.Screen name="Feeds" component={Feeds} />
+      <Stack.Screen name="Profile" component={Profile} />
+    </Stack.Navigator>
+  );
+};
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <StatusBar hidden />
+      <MyStack />
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/screens/Profile.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { Button } from "react-native-paper";
+import MyHeader from "../components/MyHeader";
+
+export default function Profile({ route, navigation }) {
+  const submit = () => {};
+  return (
+    <View style={{ flex: 1 }}>
+      <MyHeader
+        menu
+        title={route.name}
+        right="more-vertical"
+        style={styles.header}
+      />
+      <View style={styles.container}>
+        <Button mode="outlined" onPress={submit} style={{ marginTop: 20 }}>
+          Logout
+        </Button>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "rgba(52, 52, 52, 0.1)",
+    justifyContent: "center",
+    padding: 16,
+  },
+  header: {
+    borderBottomRightRadius: 16,
+    borderBottomLeftRadius: 16,
+    marginHorizontal: 4,
+  },
+});
